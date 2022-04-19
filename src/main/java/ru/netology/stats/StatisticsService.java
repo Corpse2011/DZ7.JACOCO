f81118c71928e20b394cbe4bd57b992a35d579a0
Corpse2011 <boroda_gk@bk.ru>
2022-04-19 16:37:58 +0300

    commit

--- src/main/java/ru/netology/stats/StatisticsService.java
+++ src/main/java/ru/netology/stats/StatisticsService.java
@@ -1,4 +1,4 @@
-package ru.netology.statistic;
+package ru.netology.stats;
 
 public class StatisticsService {
     /**
@@ -8,11 +8,11 @@ public class StatisticsService {
      * @return - index of first max value
      */
     public long findMax(long[] incomes) {
-        long current_max_index = 0;//вот не используется он(
+        long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
             if (current_max < income)
-                current_max = income;//некачественный код, но менять нельзя
+                current_max = income;
         return current_max;
     }
 }
--- src/test/java/ru/netology/stats/StatisticsServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -1,25 +1,32 @@
-package ru.netology.statistic;
+package ru.netology.stats;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StatisticsServiceTest {
+public class StatisticsServiceTest {
 
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
+
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
         long actual = service.findMax(incomesInBillions);
+
         assertEquals(expected, actual);
     }
+
     @Test
-    void findMaxMiddle() {
+    void findMaxInMiddle() {
         StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 35, 8, 6, 11, 11, 12};
-        long expected = 35;
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 45, 8, 6, 11, 11, 12};
+        long expected = 45;
+
         long actual = service.findMax(incomesInBillions);
+
         assertEquals(expected, actual);
     }
 }
